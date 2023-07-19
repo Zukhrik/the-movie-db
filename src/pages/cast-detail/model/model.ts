606fd713105a668aa9fde3bf91720a3197fe9178
zukhrik <kz_spiky@mail.ru>
2023-07-19 16:21:41 +0500

    fixed some warnings

--- src/pages/cast-detail/model/model.ts
+++ src/pages/cast-detail/model/model.ts
@@ -1,40 +1,40 @@
-import {createEffect, createEvent, createStore, sample} from 'effector'
+import {createEffect, createEvent, createStore, sample} from 'effector';
 import {
   getCastDetail,
   getCastImages,
   getCastTargetImages,
   ICastDetail,
   ICastImages,
-  ICastTargetImages, ITargetImages
-} from '../../../shared'
+  ITargetImages
+} from '../../../shared';
 
 //effects
 export const fetchCastDetailFx = createEffect({
   handler: getCastDetail
-})
+});
 
 export const fetchCastImagesFx = createEffect({
   handler: getCastImages
-})
+});
 
 export const fetchCastTargetImagesFx = createEffect({
   handler: getCastTargetImages
-})
+});
 
 //events
-export const opened = createEvent<number>()
-export const resetPage = createEvent()
+export const opened = createEvent<number>();
+export const resetPage = createEvent();
 
 // stores
-export const $castDetail = createStore<null | ICastDetail>(null)
-export const $castImages = createStore<null | ICastImages>(null)
-export const $castTargets = createStore<null | ITargetImages>(null)
+export const $castDetail = createStore<null | ICastDetail>(null);
+export const $castImages = createStore<null | ICastImages>(null);
+export const $castTargets = createStore<null | ITargetImages>(null);
 
-$castDetail.on(fetchCastDetailFx.doneData, (_, {data}) => data).reset(resetPage)
-$castImages.on(fetchCastImagesFx.doneData, (_, {data}) => data)
-$castTargets.on(fetchCastTargetImagesFx.doneData, (_, {data}) => data)
+$castDetail.on(fetchCastDetailFx.doneData, (_, {data}) => data).reset(resetPage);
+$castImages.on(fetchCastImagesFx.doneData, (_, {data}) => data);
+$castTargets.on(fetchCastTargetImagesFx.doneData, (_, {data}) => data);
 
 sample({
   source: opened,
   target: [fetchCastDetailFx, fetchCastImagesFx, fetchCastTargetImagesFx]
-})
+});
--- src/pages/popular/model/model.ts
+++ src/pages/popular/model/model.ts
@@ -1,21 +1,21 @@
-import {createEffect, createEvent, createStore, sample} from 'effector'
-import {getPopularMovies} from '../../../shared/api/requests'
-import {IMovieDetail, IMovieResponse} from '../../../shared/api/api.types'
+import {createEffect, createEvent, createStore, sample} from 'effector';
+import {getPopularMovies} from '../../../shared';
+import {IMovieResponse} from '../../../shared';
 
 //effects
 const fetchPopularMovieFx = createEffect({
   handler: getPopularMovies
-})
+});
 
 //events
-export const opened = createEvent()
+export const opened = createEvent();
 
 //stores
-export const $popularMovie = createStore<null | IMovieResponse>(null)
+export const $popularMovie = createStore<null | IMovieResponse>(null);
 
-$popularMovie.on(fetchPopularMovieFx.doneData, (_, {data}) => data)
+$popularMovie.on(fetchPopularMovieFx.doneData, (_, {data}) => data);
 
 sample({
   clock: opened,
   target: fetchPopularMovieFx
-})
+});
